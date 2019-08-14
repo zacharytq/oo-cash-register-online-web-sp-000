4f8a29f18bd3a076b87aa516c07430ef55ff69b4
chic-page-6502 <zacharytq@gmail.com>
2019-08-14 21:44:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+class CashRegister
+  attr_accessor :total, :discount
+  def initialize(discount)
+    @total = 0
+    self.discount = discount
+  end
 
+  def add_item(item, price, quant = 1)
+    @total += (price*quant)
+  end
+
+  def apply_discount
+    percent_discount = (@discount/100) * @total
+    puts percent_discount.to_s
+    @total = @total - percent_discount
+    puts "After the discount, the total comes to $#{@total}."
+    percent_discount
+  end
+end
